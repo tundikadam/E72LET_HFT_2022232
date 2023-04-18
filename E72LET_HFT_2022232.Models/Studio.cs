817483b85a3c4feadffed522f09ee0d442a38f66
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-04-18 21:16:06 +0200

    Created Studio class

--- E72LET_HFT_2022232.Models/Studio.cs
+++ E72LET_HFT_2022232.Models/Studio.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace E72LET_HFT_2022232.Models
 {
-    internal class Studio
+    public class Studio
     {
+        public Studio(int studioId, int studioName)
+        {
+            StudioId = studioId;
+            StudioName = studioName;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        
+        int StudioId { get; set; }
+        [StringLength(50)]
+        int StudioName { get; set; }
+
+
     }
 }
+
