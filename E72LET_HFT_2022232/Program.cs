14fbb8a905ec2bd2a978a7d29de3e3872fc6fe4e
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-04-18 11:50:40 +0200

    Add project files.

---
+++ E72LET_HFT_2022232/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E72LET_HFT_2022232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
