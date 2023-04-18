591455099154f72f66a2472ba35ff6d16e8f9c48
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-04-18 21:20:18 +0200

    Key setted

--- E72LET_HFT_2022232.Models/Games.cs
+++ E72LET_HFT_2022232.Models/Games.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace E72LET_HFT_2022232.Models
 {
-    public class Class1
+    public class Games
     {
+        public Games(string name, int age_Limit, int price, int id)
+        {
+            Name = name;
+            Age_Limit = age_Limit;
+            Price = price;
+            Id = id;
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Range(0,18)]
+        public int Age_Limit { get; set; }
+
+
+        //Az ár euróban értendő
+        [Range(0, 100)]
+        public int Price { get; set; } 
+
+    
+
+
     }
 }
