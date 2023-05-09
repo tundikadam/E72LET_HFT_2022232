4deb638acb4ed6cdbea65bbdde0bc06ab7248219
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-09 14:33:27 +0200

    GameDbContext OnConfiguring

--- E72LET_HFT_2022232.Repository/Class1.cs
+++ E72LET_HFT_2022232.Repository/Class1.cs
@@ -4,23 +4,25 @@ using System;
 
 namespace E72LET_HFT_2022232.Repository
 {
-    public class GameDbContext:DbContext
-    {  public DbSet<Games> Games { get; set; }
+    public class GameDbContext : DbContext
+    {
+        public DbSet<Games> Games { get; set; }
         public DbSet<MinimalSystemRequirements> MinimalSystemRequirements { get; set; }
         public DbSet<Studio> Studios { get; set; }
 
-       public GameDbContext()
+        public GameDbContext()
         { Database.EnsureCreated(); }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if(!optionsBuilder.IsConfigured)
+            if (!optionsBuilder.IsConfigured)
             {
+                string conn = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =G:\Egyetem\4\Haladó fejlesztési technikák\Féléves\Feladat\E72LET_HFT_2022232\E72LET_HFT_2022232.Repository\games.mdf; Integrated Security = True;MultipleActiveResultSets=True";
                 optionsBuilder.UseSqlServer
-                    ().UseLazyLoadingProxies();
+                    (conn).UseLazyLoadingProxies();
             }
+
+
+
         }
-
-
-
     }
 }
