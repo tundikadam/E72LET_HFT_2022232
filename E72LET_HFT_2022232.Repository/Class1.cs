0588c3f38f7c49bd5367c0dd18d5a0563aa95ee8
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-09 14:23:10 +0200

    Database created

--- E72LET_HFT_2022232.Repository/Class1.cs
+++ E72LET_HFT_2022232.Repository/Class1.cs
@@ -7,8 +7,18 @@ namespace E72LET_HFT_2022232.Repository
     public class GameDbContext:DbContext
     {  public DbSet<Games> Games { get; set; }
         public DbSet<MinimalSystemRequirements> MinimalSystemRequirements { get; set; }
+        public DbSet<Studio> Studios { get; set; }
+
        public GameDbContext()
         { Database.EnsureCreated(); }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer
+                    ().UseLazyLoadingProxies();
+            }
+        }
 
 
 
