3234ac97f00f48a3f01542f683062732dd7b0602
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-11 13:17:36 +0200

    tested in memorydatabase

--- E72LET_HFT_2022232/Program.cs
+++ E72LET_HFT_2022232/Program.cs
@@ -1,4 +1,5 @@
-﻿using E72LET_HFT_2022232.Repository;
+﻿using E72LET_HFT_2022232.Models;
+using E72LET_HFT_2022232.Repository;
 using System;
 using System.Linq;
 
@@ -9,9 +10,15 @@ namespace E72LET_HFT_2022232
         static void Main(string[] args)
         {
             GameDbContext db = new GameDbContext();
-            var games = db.Games.ToArray();
-            var studios = db.Studios.ToArray();
-            var minimalsystem = db.MinimalSystemRequirements.ToArray();
+            //  var games = db.Games.ToArray();
+            // var studios = db.Studios.ToArray();
+            //var minimalsystem = db.MinimalSystemRequirements.ToArray();
+            IRepository<Studio> repository = new StudioRepository(db);
+            Studio s = new Studio(100, "AdamSoft");
+            repository.Create(s);
+            repository.Delete(100);
+            var items = repository.ReadAll().ToArray();
+            ;
             ;
         }
     }
