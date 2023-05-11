ba154bebd93cbfde2121615c27369efa43c601ac
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-11 21:51:42 +0200

    Extracted Studio Logic to IStudioLogic

---
+++ E72LET_HFT_2022232.Logic/IStudioLogic.cs
@@ -0,0 +1,14 @@
+﻿using E72LET_HFT_2022232.Models;
+using System.Linq;
+
+namespace E72LET_HFT_2022232.Logic
+{
+    public interface IStudioLogic
+    {
+        void Create(Studio item);
+        void Delete(int id);
+        Studio Read(int id);
+        IQueryable<Studio> ReadAll();
+        void Update(Studio item);
+    }
+}
--- E72LET_HFT_2022232.Logic/StudioLogic.cs
+++ E72LET_HFT_2022232.Logic/StudioLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using E72LET_HFT_2022232.Models;
+using E72LET_HFT_2022232.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,39 @@ using System.Threading.Tasks;
 
 namespace E72LET_HFT_2022232.Logic
 {
-    internal class StudioLogic
+    public class StudioLogic : IStudioLogic
     {
+        IRepository<Studio> repo;
+        public void Create(Studio item)
+        {
+            if (item.StudioName.Length < 5)
+            { throw new ArgumentException("The studio's name is too short"); }
+
+            this.repo.Create(item);
+
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Studio Read(int id)
+        {
+            var studio = this.repo.Read(id);
+            if (studio == null)
+            { throw new ArgumentException("Studio not exists"); }
+            return studio;
+        }
+
+        public IQueryable<Studio> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Studio item)
+        {
+            repo.Update(item);
+        }
     }
 }
