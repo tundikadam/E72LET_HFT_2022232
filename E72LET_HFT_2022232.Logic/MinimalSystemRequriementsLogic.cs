6286f5d06cd9e82db67f75c59beefd1888952acd
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-11 22:00:21 +0200

    Create minimal system requriementsLogic

---
+++ E72LET_HFT_2022232.Logic/MinimalSystemRequriementsLogic.cs
@@ -0,0 +1,39 @@
+﻿using E72LET_HFT_2022232.Models;
+using E72LET_HFT_2022232.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E72LET_HFT_2022232.Logic
+{
+    internal class MinimalSystemRequriementsLogic 
+    {
+        IRepository<MinimalSystemRequirements> repo;
+        public void Create(MinimalSystemRequirements item)
+        {
+            repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public MinimalSystemRequirements Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<MinimalSystemRequirements> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(MinimalSystemRequirements item)
+        {
+            repo.Update(item);
+        }
+    }
+}
