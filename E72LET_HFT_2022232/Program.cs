ed06fb279f57c7f623c019152165a7d261219f5e
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-11 21:38:17 +0200

    Tested Game logic

--- E72LET_HFT_2022232/Program.cs
+++ E72LET_HFT_2022232/Program.cs
@@ -1,4 +1,5 @@
-﻿using E72LET_HFT_2022232.Models;
+﻿using E72LET_HFT_2022232.Logic;
+using E72LET_HFT_2022232.Models;
 using E72LET_HFT_2022232.Repository;
 using System;
 using System.Linq;
@@ -9,8 +10,21 @@ namespace E72LET_HFT_2022232
     {
         static void Main(string[] args)
         {
-          
-           
+            var ctx = new GameDbContext();
+            var repo = new GameRepository(ctx);
+            var logic = new GameLogic(repo);
+            
+            Game g = new Game(100, 100, 100, "Honfoglalo", 3, 1000, 10);
+            logic.Create(g);
+            
+            logic.Delete(100);
+            var items = logic.ReadAll().ToArray();
+            var activision = logic.ActivisionsGamePriceAverage();
+            var contendo = logic.ContendoCount();
+             ;
+
+
+
         }
     }
 }
