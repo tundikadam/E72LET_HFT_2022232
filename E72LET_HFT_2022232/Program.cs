4853acc084f08757c977a7be4a0a4576d0ba7642
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-11 13:17:50 +0200

    deleted test code

--- E72LET_HFT_2022232/Program.cs
+++ E72LET_HFT_2022232/Program.cs
@@ -9,17 +9,8 @@ namespace E72LET_HFT_2022232
     {
         static void Main(string[] args)
         {
-            GameDbContext db = new GameDbContext();
-            //  var games = db.Games.ToArray();
-            // var studios = db.Studios.ToArray();
-            //var minimalsystem = db.MinimalSystemRequirements.ToArray();
-            IRepository<Studio> repository = new StudioRepository(db);
-            Studio s = new Studio(100, "AdamSoft");
-            repository.Create(s);
-            repository.Delete(100);
-            var items = repository.ReadAll().ToArray();
-            ;
-            ;
+          
+           
         }
     }
 }
