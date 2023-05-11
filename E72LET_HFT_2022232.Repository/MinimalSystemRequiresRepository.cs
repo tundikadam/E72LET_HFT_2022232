ea5b7892eebaccda9861b942d09774a38ed0d4e0
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-11 12:55:24 +0200

    MinimalSystemReuuries repository created

--- E72LET_HFT_2022232.Repository/MinimalSystemRequiresRepository.cs
+++ E72LET_HFT_2022232.Repository/MinimalSystemRequiresRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E72LET_HFT_2022232.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace E72LET_HFT_2022232.Repository
 {
-    internal class MinimalSystemRequiresRepository
+    public class MinimalSystemRequiresRepository : Repository<MinimalSystemRequirements>, IRepository<MinimalSystemRequirements>
     {
+        public MinimalSystemRequiresRepository(GameDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override MinimalSystemRequirements Read(int id)
+        {
+            return ctx.MinimalSystemRequirements.FirstOrDefault(t=>t.MinimalSystemRequirementsId==id);
+        }
+
+        public override void Update(MinimalSystemRequirements item)
+        {
+            var old = Read(item.MinimalSystemRequirementsId);
+            foreach (var prop in old.GetType().GetProperties())
+            { prop.SetValue(old, prop.GetValue(item)); }
+            ctx.SaveChanges();
+
+        }
     }
 }
