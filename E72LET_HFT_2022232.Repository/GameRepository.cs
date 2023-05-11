06a8e051e8ace50057afd413f6a2ba9fb2f35d52
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-11 12:56:40 +0200

    Created Repositories folder

--- E72LET_HFT_2022232.Repository/GameRepository.cs
+++ E72LET_HFT_2022232.Repository/GameRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using E72LET_HFT_2022232.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace E72LET_HFT_2022232.Repository
 {
-    internal class GameRepository
+    public class GameRepository : Repository<Game>, IRepository<Game>
     {
+        public GameRepository(GameDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Game Read(int id)
+        {
+            return ctx.Games.FirstOrDefault(t => t.Id == id);
+        }
+
+        public override void Update(Game item)
+        {
+            var old = Read(item.Id);
+            foreach(var prop in old.GetType().GetProperties())
+            { prop.SetValue(old, prop.GetValue(item)); }
+            ctx.SaveChanges();
+        }
     }
 }
--- E72LET_HFT_2022232.Repository/StudioRepository.cs
+++ E72LET_HFT_2022232.Repository/StudioRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E72LET_HFT_2022232.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace E72LET_HFT_2022232.Repository
 {
-    internal class StudioRepository
+    public class StudioRepository : Repository<Studio>, IRepository<Studio>
     {
+        public StudioRepository(GameDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Studio Read(int id)
+        {
+            return ctx.Studios.FirstOrDefault(t => t.StudioId == id);
+        }
+
+        public override void Update(Studio item)
+        {
+            var old = Read(item.StudioId);
+            foreach (var prop in old.GetType().GetProperties())
+            { prop.SetValue(old, prop.GetValue(item)); }
+            ctx.SaveChanges();
+        }
     }
 }
