412e9038b14081146cc69db097eb1c170dd65bf7
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-05-11 22:01:38 +0200

    Extract IMinimalSytemRequriementsLogic interface

---
+++ E72LET_HFT_2022232.Logic/IMinimalSystemRequriementsLogic.cs
@@ -0,0 +1,14 @@
+﻿using E72LET_HFT_2022232.Models;
+using System.Linq;
+
+namespace E72LET_HFT_2022232.Logic
+{
+    internal interface IMinimalSystemRequriementsLogic
+    {
+        void Create(MinimalSystemRequirements item);
+        void Delete(int id);
+        MinimalSystemRequirements Read(int id);
+        IQueryable<MinimalSystemRequirements> ReadAll();
+        void Update(MinimalSystemRequirements item);
+    }
+}
