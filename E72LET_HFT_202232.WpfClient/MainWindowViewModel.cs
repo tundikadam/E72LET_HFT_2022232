60747abb3f94bf97bb6944eb74796ca17c867220
tundik.adam <adamtundik@stud.uni-obuda.hu>
2024-04-02 13:44:21 +0200

    UI layer completed

---
+++ E72LET_HFT_202232.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using E72LET_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E72LET_HFT_202232.WpfClient
+{
+  public class MainWindowViewModel
+    {
+        public RestCollection<Game> Games { get; set; }
+        public MainWindowViewModel()
+        {
+            Games = new RestCollection<Game>("http://localhost:18902/", "swagger");
+        }
+    }
+}
